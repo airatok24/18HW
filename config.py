fb13f5d5b753f14d527c5851d929f13f5d43a12e
airatok24 <101453810+airatok24@users.noreply.github.com>
2022-06-06 11:07:05 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,4 @@
+class Config(object):
+    DEBUG = True
+    SQL_ALCHEMY_DATABASE_URi = 'sqlite:///movies.db'
+    SQL_ALCHEMY_TRACK_MODIFICATION = False
---
+++ implemented.py
@@ -0,0 +1,10 @@
+# файл для создания DAO и сервисов чтобы импортировать их везде
+from dao.movies import MoviesDAO
+from service.movies import MoviesService
+from setup_db import db
+
+movies_dao = MoviesDAO(db.session)
+movies_service = MoviesService(movies_dao=movies_dao)
+
+#review_dao = ReviewDAO(db.session)
+#review_service = ReviewService(dao=review_dao)
